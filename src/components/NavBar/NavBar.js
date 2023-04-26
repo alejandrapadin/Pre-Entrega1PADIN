d566f47d7d29092630eab0ba320118c9851fb8ec
alejandrapadin <alejandrarpg@gmail.com>
2023-04-25 22:05:32 -0300

    repo
    commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>PRIMERA ENTREGA</h1>
+            <div>
+                <button>Click Aquí</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
